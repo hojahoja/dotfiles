ff77ac33e520e2e67d5339dc0ffc0af8aecd570d
hojahoja <5722145+hojahoja@users.noreply.github.com>
2026-03-09 22:26:36 +0200

    Crossnote class and new fzf.fish binding
    - Added class for crossnote for centering images in markdown preview
      enhanced
    - Separate binding for fzf.fish for including hidden files into
      directory search.

--- home/dot_config/private_fish/conf.d/fzf_custom.fish
+++ home/dot_config/private_fish/conf.d/fzf_custom.fish
@@ -1,1 +1,20 @@
+# bind git log searchto CTRL+ALT+G
 fzf_configure_bindings --git_log=\e\cg
+
+# Custom function to enable hidden file search
+function _fzf_search_directory_hidden
+
+    # Store current search options
+    set -l old_fd_opts $fzf_fd_opts
+
+    # Set search options to look for hidden files
+    set -g fzf_fd_opts --hidden
+
+    _fzf_search_directory
+
+    # Restore old options
+    set -g fzf_fd_opts $old_fd_opts
+end
+
+# bind hidden file search to CTRL+ALT+D
+bind \e\cd _fzf_search_directory_hidden
